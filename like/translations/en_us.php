18b8aee743f02a9d54f62864a0a66703e17221f0
Benjamin David <ben@dukt.net>
2014-01-03 00:05:12 +0100

    translations

--- like/translations/en_us.php
+++ like/translations/en_us.php
@@ -4,17 +4,17 @@ return array(
 
     // Like Entries
 
-    "like.likeentries_setting_label" => "Notify me when someone likes my assets",
-    "like.likeentries_heading" => "When someone likes an entry:",
-    "like.likeentries_subject" => "Someone has liked one of your entries",
-    "like.likeentries_body"    => "{{user.friendlyName}} has liked one of your entries :\n\n".
-                                  "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}"
-
+    "like.onlikeentries_settings_label" => "Notify me when someone likes my entries",
+    "like.onlikeentries_heading"        => "When someone likes an entry:",
+    "like.onlikeentries_subject"        => "Someone has liked one of your entries",
+    "like.onlikeentries_body"           => "{{user.friendlyName}} has liked one of your entries :\n\n".
+                                         "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}",
 
 
     // LikesMe
 
-    "like.likesme_heading" => "When someone is getting liked:",
-    "like.likesme_subject" => "Someone likes you",
-    "like.likesme_body"    => "{{user.friendlyName}} likes you.",
+    "like.onlikeme_settings_label"     => "Notify me when someone likes me",
+    "like.onlikeme_heading"            => "When someone is getting liked:",
+    "like.onlikeme_subject"            => "Someone likes you",
+    "like.onlikeme_body"               => "{{user.friendlyName}} likes you.",
 );
