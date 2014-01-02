531b67877fd310c6577036d875be3f87c2aec8e4
Benjamin David <ben@dukt.net>
2014-01-02 17:30:20 +0100

    translations

--- like/translations/en_us.php
+++ like/translations/en_us.php
@@ -7,23 +7,21 @@ return array(
     "like.likeentries_setting_label" => "Notify me when someone likes my assets",
     "like.likeentries_heading" => "When someone likes an entry:",
     "like.likeentries_subject" => "Someone has liked one of your entries",
-    "like.likeentries_body"    => "A user has liked one of your entries :
-                                    - user : {{user.username}}
-                                    - entry : {{(entry.url is defined ? entry.url : entry.id)}}",
+    "like.likeentries_body"    => "{{user.friendlyName}} has liked one of your entries :\n\n".
+                                  "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}"
 
 
     // Like Assets
 
     "like.likeassets_heading" => "When someone likes an asset:",
     "like.likeassets_subject" => "Someone has liked one of your assets",
-    "like.likeassets_body"    => "A user has liked one of your assets :
-                                    - user : {{user.username}}
-                                    - asset : {{(asset.url is defined ? asset.url : asset.id)}}",
+    "like.likeassets_body"    => "{{user.friendlyName}} has liked one of your assets :\n\n".
+                                 "{{(asset.url is defined ? asset.url : 'Asset ID:'~asset.id)}}",
 
 
     // LikesMe
 
     "like.likesme_heading" => "When someone is getting liked:",
     "like.likesme_subject" => "Someone likes you",
-    "like.likesme_body"    => "{{user.username}} likes you.",
+    "like.likesme_body"    => "{{user.friendlyName}} likes you.",
 );
