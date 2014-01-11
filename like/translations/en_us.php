36bc440942d453f6c8f6a4eca2d2976ec4e18220
Benjamin David <ben@dukt.net>
2014-01-11 17:33:50 +0100

    translations

--- like/translations/en_us.php
+++ like/translations/en_us.php
@@ -7,8 +7,7 @@ return array(
     "like.onlikeentries_settings_label" => "Notify me when someone likes my entries",
     "like.onlikeentries_heading"        => "When someone likes an entry:",
     "like.onlikeentries_subject"        => "Someone has liked one of your entries",
-    "like.onlikeentries_body"           => "{{user.friendlyName}} has liked one of your entries :\n\n".
-                                         "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}",
+    "like.onlikeentries_body"           => "Hello {{user.friendlyName}},\n\n{{contextUser.friendlyName}} has liked one of your entries :\n\n"."{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}",
 
 
     // LikesMe
@@ -16,5 +15,5 @@ return array(
     "like.onlikeme_settings_label"     => "Notify me when someone likes me",
     "like.onlikeme_heading"            => "When someone is getting liked:",
     "like.onlikeme_subject"            => "Someone likes you",
-    "like.onlikeme_body"               => "{{user.friendlyName}} likes you.",
+    "like.onlikeme_body"               => "Hello {{user.friendlyName}},\n\n{{contextUser.friendlyName}} likes you.",
 );
